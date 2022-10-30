c5a502d8ec345ac5419846ac5d6c5cc2931d08f8
adamKabbara <adamkabbara6@gmail.com>
2022-10-30 23:18:25 +1100

    Changed Link to link

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -12,7 +12,9 @@ export default function PostPage({
   return (
     <>
       <div className="p-5 pt-1" id="slug-main-container">
-        <h1 className="text-4xl">{title}</h1>
+        <h1 className="text-4xl" id="post-title">
+          {title}
+        </h1>
         <div className=" leading-9 text-slate-500 text-lg">{date}</div>
         <img
           src={thumbnail}
@@ -21,7 +23,7 @@ export default function PostPage({
         />
         <div
           style={{ width: '100%' }}
-          className="prose prose-headings:text-white prose-p:text-white prose-li:text-white prose-a:text-white prose-strong:text-white  prose-headings max-w-none"
+          className="prose-base prose-h1:text-3xl prose-headings:text-white prose-p:text-white prose-li:text-white prose-a:text-white prose-strong:text-white  prose-headings max-w-none"
           id="main"
         >
           <div
