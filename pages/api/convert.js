099f15e1bcab07066201b6b82539bd76eefa4b12
Adam <Adam@Mrs-MacBook-Pro.local>
2023-05-10 08:59:18 +1000

    progressed post creation page

--- pages/api/convert.js
+++ pages/api/convert.js
@@ -9,52 +9,59 @@ import * as fs from 'fs'
 
 import path from 'path'
 export default function handler(req, res) {
+  if (req.method !== 'POST') return
+
+  // const body = JSON.parse(req.body)
+  console.log(req.body)
+
   const wordsApi = new WordsApi(
     'af80956f-7ce5-4f2e-a273-b1ca72654d9b',
     '09de9df66660ec7e361903022078e08d'
   )
 
-  const requestDocument = fs.createReadStream(
-    path.join(__dirname, '../../../../files/cms-test.docx')
-  )
-  const convertRequest = new ConvertDocumentRequest({
-    document: requestDocument,
-    format: 'md',
-  })
-
-  wordsApi.convertDocument(convertRequest).then((convertRequestResult) => {
-    // tslint:disable-next-line:no-console
-    console.log(convertRequest)
-    fs.writeFileSync('test.md', convertRequestResult.body)
-  })
-  // const uploadRequest = new UploadFileRequest()
-  // uploadRequest.path = 'cms-test.docx'
-  // uploadRequest.fileContent = createReadStream(
+  // const requestDocument = fs.createReadStream(
   //   path.join(__dirname, '../../../../files/cms-test.docx')
   // )
 
-  // wordsApi.uploadFile(uploadRequest).then((_uploadResult) => {
-  //   // save the file as pdf in the cloud
-  //   const request = new SaveAsRequest({
-  //     name: 'cms-test.docx',
-  //     saveOptionsData: new PdfSaveOptionsData({
-  //       fileName: 'destination.pdf',
-  //     }),
-  //   })
+  // const requestDocument = req.body
 
-  //   wordsApi
-  //     .saveAs(request)
-  //     .then((_result) => {
-  //       console.log(_result)
-  //       // deal with the pdf file
-  //     })
-  //     .catch(function (_err) {
-  //       // handle saveAs request error
-  //     })
-  // })
-  // .catch(function (_err) {
-  //   // handle uploadFile request error
+  // const convertRequest = new ConvertDocumentRequest({
+  //   document: requestDocument,
+  //   format: 'md',
   // })
 
-  res.status(200).json({ name: 'John Doe' })
+  // wordsApi.convertDocument(convertRequest).then((convertRequestResult) => {
+  //   fs.writeFileSync('testing files/new file.md', convertRequestResult.body)
+  // })
+
+  // res.status(200).json({ name: 'John Doe' })
 }
+
+// const uploadRequest = new UploadFileRequest()
+// uploadRequest.path = 'cms-test.docx'
+// uploadRequest.fileContent = createReadStream(
+//   path.join(__dirname, '../../../../files/cms-test.docx')
+// )
+
+// wordsApi.uploadFile(uploadRequest).then((_uploadResult) => {
+//   // save the file as pdf in the cloud
+//   const request = new SaveAsRequest({
+//     name: 'cms-test.docx',
+//     saveOptionsData: new PdfSaveOptionsData({
+//       fileName: 'destination.pdf',
+//     }),
+//   })
+
+//   wordsApi
+//     .saveAs(request)
+//     .then((_result) => {
+//       console.log(_result)
+//       // deal with the pdf file
+//     })
+//     .catch(function (_err) {
+//       // handle saveAs request error
+//     })
+// })
+// .catch(function (_err) {
+//   // handle uploadFile request error
+// })
--- pages/posts/create.js
+++ pages/posts/create.js
@@ -1,50 +1,73 @@
+import { useEffect, useRef } from 'react'
+
 function Create() {
+  const fileInput = useRef(null)
+
+  const uploadFile = () => {
+    const file = fileInput.current.files[0]
+
+    var data = new FormData()
+    data.append('file', file)
+    console.log(data.get('file'))
+    fetch('/api/convert', {
+      method: 'POST',
+      body: data,
+    })
+    // .then((response) => response.json())
+    // .then((data) => {
+    //   console.log(data)
+    // })
+  }
+
   return (
     <div className="mt-20 w-full flex items-center justify-center">
-      {/* <p className="w-5 p-5">test</p> */}
-
-      <form className="w-1/2 max-w-260 ">
+      <form
+        className="w-1/2 max-w-260 max-sm:w-full max-md:w-2/3"
+        onSubmit={(e) => {
+          e.preventDefault()
+          uploadFile()
+        }}
+      >
         {/* <div class="flex flex-wrap -mx-3 mb-6"> */}
-        <div class="w-full px-3 mb-6 md:mb-0">
+        <div className="w-full px-3 mb-6 md:mb-0">
           <label
-            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            for="grid-first-name"
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="grid-first-name"
           >
             First Name
           </label>
           <input
-            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white min-w-fit mt-3"
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white min-w-fit mt-3"
             id="grid-first-name"
             type="text"
             placeholder="Jane"
           />
         </div>
-        <div class="w-full  px-3 mb-6 md:mb-0 mt-6">
+        <div className="w-full  px-3 mb-6 md:mb-0 mt-6">
           <label
-            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            for="grid-last-name"
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="grid-last-name"
           >
             Last Name
           </label>
           <input
-            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 mb-3 mt-3"
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 mb-3 mt-3"
             id="grid-last-name"
             type="text"
             placeholder="Doe"
           />
-          {/* </div> */}
         </div>
 
-        <div class="w-full px-3 mb-6 md:mb-0 mt-6">
+        <div className="w-full px-3 mb-6 md:mb-0 mt-6">
           <label
-            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            for="grid-state"
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="grid-state"
           >
             Category
           </label>
-          <div class="relative ">
+          <div className="relative ">
             <select
-              class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded  focus:outline-none focus:bg-white focus:border-gray-500 mt-3"
+              className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded  focus:outline-none focus:bg-white focus:border-gray-500 mt-3"
               id="grid-state"
             >
               <option>Information Technology</option>
@@ -52,9 +75,9 @@ function Create() {
               <option>Engineering</option>
               <option>Medicine</option>
             </select>
-            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
               <svg
-                class="fill-current h-4 w-4"
+                className="fill-current h-4 w-4"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 20 20"
               >
@@ -62,6 +85,29 @@ function Create() {
               </svg>
             </div>
           </div>
+        </div>
+        <div className="w-full  px-3 mb-6 md:mb-0 mt-6">
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="file_input"
+          >
+            Upload file
+          </label>
+          <input
+            className="  block w-full text-sm text-gray-900 border border-gray-300 rounded bg-gray-200 text-gray-700 border border-gray-200 rounded  leading-tight focus:outline-none focus:bg-white focus:border-gray-500 mb-3 mt-3"
+            id="uploadedFile"
+            type="file"
+            ref={fileInput}
+            required
+          />
+        </div>
+        <div className="w-full px-3 mb-6 md:mb-0 mt-6">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Sign In
+          </button>
         </div>
       </form>
     </div>
