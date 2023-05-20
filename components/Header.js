cc662ad7637d4c9f786d9df72da17d57b33a9101
Adam <Adam@Mrs-MacBook-Pro.local>
2023-05-20 15:58:37 +1000

    Clicking about and contact closes menu

--- components/Header.js
+++ components/Header.js
@@ -75,7 +75,6 @@ export default function Header() {
                   <a
                     href="../posts/create"
                     className=" block py-2 pr-4 pl-3 text-gray-700 rounded   md:border-0  md:p-0 dark:text-gray-100   "
-                    onClick={() => console.log(isAuthenticated)}
                   >
                     Create Post
                   </a>
@@ -102,16 +101,17 @@ export default function Header() {
                   </a>
                 )}
               </li>
-              <li>
+              <li onClick={() => setIsToggled(!isToggled)}>
                 <Link
                   href="about"
                   className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  onClick={() => setIsToggled(!isToggled)}
                 >
                   About
                 </Link>
               </li>
 
-              <li>
+              <li onClick={() => setIsToggled(!isToggled)}>
                 <Link
                   href="/contact"
                   className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
