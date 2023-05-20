6ca302f13f37e8fcbbc196cca6765817fabe0440
Adam <Adam@Mrs-MacBook-Pro.local>
2023-05-20 15:34:53 +1000

    fixed build linting errors

--- components/Header.js
+++ components/Header.js
@@ -21,7 +21,7 @@ export default function Header() {
     toggleMenu(menuRef)
   }, [isToggled])
 
-  const imageLink = '/icons/icon4.svg'
+  const imageLink = /* '/academic-cms' + */ '/icons/icon4.svg'
 
   const { loginWithRedirect, user, isAuthenticated, logout } = useAuth0()
 
@@ -103,21 +103,21 @@ export default function Header() {
                 )}
               </li>
               <li>
-                <a
-                  href="/about"
+                <Link
+                  href="about"
                   className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   About
-                </a>
+                </Link>
               </li>
 
               <li>
-                <a
+                <Link
                   href="/contact"
                   className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Contact
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
