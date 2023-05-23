28a33e0c53a28e9161bffbe5b04bff4f0568a94b
Adam Kabbara <adamkabbara6@gmail.com>
2023-05-23 14:19:53 +1000

    Added save as pdf button

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,16 @@
 function About() {
-  return <div>About</div>;
+  return (
+    <div className="pl-5 pr-5 main-content pt-20">
+      <h1 className="font-bold pb-10 pt-20 text-3xl">About Us</h1>
+      <p className="text-xl">
+        We are a startup aiming to research and deliver the ultimate content
+        management system to provide researchers and academic a platform by
+        which they are able to adequately spread their research and work
+      </p>
+      <br />
+      <p className="text-xl"></p>
+    </div>
+  )
 }
 
-export default About;
+export default About
--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,16 @@
 function Contact() {
-  return <div>Contact</div>
+  return (
+    <div className="main-content pt-20">
+      <h1 className="font-bold pb-10 pt-20 text-3xl">Contact Us</h1>
+      <p className="text-xl">
+        If any enquiries or issues arise please feel free to email us at
+        <span className="font-bold s"> academic-cms@gmail.com</span> and expect
+        a reply within 48 hours.{' '}
+      </p>
+      <br />
+      <p className="text-xl">Thank You.</p>
+    </div>
+  )
 }
 
 export default Contact
