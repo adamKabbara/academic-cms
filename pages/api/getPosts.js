dc5031796f0714ebefe03ca952b9974bd4e6265f
Adam <Adam@Mrs-MacBook-Pro.local>
2023-05-29 23:47:54 +1000

    default

--- pages/api/getPosts.js
+++ pages/api/getPosts.js
@@ -1,9 +1,10 @@
 import connectDB from '../../utils/connectDB'
 import { MongoClient } from 'mongodb'
 
-export async function getPosts(req, res) {
+export default async function getPosts(req, res) {
+  console.log('conn')
   const client = await connectDB()
-
+  console.log('asdjk;fasdjf;')
   const send = await client
     .db('Project0')
     .collection('posts')
