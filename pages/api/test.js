2ee56a9a47f3de40c1f174d35b0892917f427e24
Adam <Adam@Mrs-MacBook-Pro.local>
2023-05-30 00:22:56 +1000

    sdfa

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,12 +1,13 @@
 import { MongoClient, ServerApiVersion } from 'mongodb'
 import connectDB from '../../utils/connectDB'
 
-export default function test(req, res) {
+export default async function test(req, res) {
   let prom = () =>
     new Promise((resolve, reject) => {
       setTimeout(() => resolve('asdf'), 1000)
     })
 
-  prom().then((res) => console.log(res))
+  const p = await prom()
+  console.log(p)
   res.json({ test: 'is this working' })
 }
