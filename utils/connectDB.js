e33b7fd39df97f0c4a7d772297df3b2631e2d26b
Adam <Adam@Mrs-MacBook-Pro.local>
2023-05-29 23:16:53 +1000

    URI

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -2,7 +2,9 @@ import { MongoClient } from 'mongodb'
 
 export default function connectDB() {
   const uri =
-    'mongodb://adamkabbara123:adamkabbara123@ac-uenkeqs-shard-00-00.dfotkzq.mongodb.net:27017,ac-uenkeqs-shard-00-01.dfotkzq.mongodb.net:27017,ac-uenkeqs-shard-00-02.dfotkzq.mongodb.net:27017/?ssl=true&replicaSet=atlas-assqju-shard-0&authSource=admin&retryWrites=true&w=majority'
+    process.env.NODE_ENV === 'development'
+      ? 'mongodb://adamkabbara123:adamkabbara123@ac-uenkeqs-shard-00-00.dfotkzq.mongodb.net:27017,ac-uenkeqs-shard-00-01.dfotkzq.mongodb.net:27017,ac-uenkeqs-shard-00-02.dfotkzq.mongodb.net:27017/?ssl=true&replicaSet=atlas-assqju-shard-0&authSource=admin&retryWrites=true&w=majority'
+      : process.env.MONGODB_URI
 
   const client = new MongoClient(uri)
   client
