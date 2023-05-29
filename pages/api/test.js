3ac4ea09a61526c895e12728b811ca75a8bcc237
Adam <Adam@Mrs-MacBook-Pro.local>
2023-05-30 00:20:00 +1000

    tewste

--- pages/api/test.js
+++ pages/api/test.js
@@ -2,5 +2,11 @@ import { MongoClient, ServerApiVersion } from 'mongodb'
 import connectDB from '../../utils/connectDB'
 
 export default function test(req, res) {
+  let prom = () =>
+    new Promise((resolve, reject) => {
+      setTimeout(() => resolve('asdf'), 1000)
+    })
+
+  prom().then((res) => console.log(res))
   res.json({ test: 'is this working' })
 }
